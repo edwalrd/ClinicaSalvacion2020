5abca620fc9d4ca5294122ee468d28d23f5499bf
edwal <edwallg@gmail.com>
2020-04-06 19:06:45 -0400

    Controlador Ingreso creado a su totalidad

--- clinicasalvacion2020/clinicasalvacion2020/Controllers/IngresosController.cs
+++ clinicasalvacion2020/clinicasalvacion2020/Controllers/IngresosController.cs
@@ -22,24 +22,12 @@ namespace clinicasalvacion2020.Controllers
         }
 
         // GET: Ingresos/Details/5
-        public ActionResult Details(int? id)
-        {
-            if (id == null)
-            {
-                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
-            }
-            Ingresos ingresos = db.Ingresos.Find(id);
-            if (ingresos == null)
-            {
-                return HttpNotFound();
-            }
-            return View(ingresos);
-        }
+
 
         // GET: Ingresos/Create
         public ActionResult Create()
         {
-            ViewBag.habitacionesid = new SelectList(db.Habitaciones, "id", "id");
+            ViewBag.habitacionesid = new SelectList(db.Habitaciones, "id", "numero");
             ViewBag.pacienteid = new SelectList(db.Pacientes, "id", "nombre");
             return View();
         }
@@ -61,67 +49,6 @@ namespace clinicasalvacion2020.Controllers
             ViewBag.habitacionesid = new SelectList(db.Habitaciones, "id", "id", ingresos.habitacionesid);
             ViewBag.pacienteid = new SelectList(db.Pacientes, "id", "nombre", ingresos.pacienteid);
             return View(ingresos);
-        }
-
-        // GET: Ingresos/Edit/5
-        public ActionResult Edit(int? id)
-        {
-            if (id == null)
-            {
-                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
-            }
-            Ingresos ingresos = db.Ingresos.Find(id);
-            if (ingresos == null)
-            {
-                return HttpNotFound();
-            }
-            ViewBag.habitacionesid = new SelectList(db.Habitaciones, "id", "id", ingresos.habitacionesid);
-            ViewBag.pacienteid = new SelectList(db.Pacientes, "id", "nombre", ingresos.pacienteid);
-            return View(ingresos);
-        }
-
-        // POST: Ingresos/Edit/5
-        // Para protegerse de ataques de publicación excesiva, habilite las propiedades específicas a las que desea enlazarse. Para obtener 
-        // más información vea https://go.microsoft.com/fwlink/?LinkId=317598.
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public ActionResult Edit([Bind(Include = "id,fecha_inicio,pacienteid,habitacionesid")] Ingresos ingresos)
-        {
-            if (ModelState.IsValid)
-            {
-                db.Entry(ingresos).State = EntityState.Modified;
-                db.SaveChanges();
-                return RedirectToAction("Index");
-            }
-            ViewBag.habitacionesid = new SelectList(db.Habitaciones, "id", "id", ingresos.habitacionesid);
-            ViewBag.pacienteid = new SelectList(db.Pacientes, "id", "nombre", ingresos.pacienteid);
-            return View(ingresos);
-        }
-
-        // GET: Ingresos/Delete/5
-        public ActionResult Delete(int? id)
-        {
-            if (id == null)
-            {
-                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
-            }
-            Ingresos ingresos = db.Ingresos.Find(id);
-            if (ingresos == null)
-            {
-                return HttpNotFound();
-            }
-            return View(ingresos);
-        }
-
-        // POST: Ingresos/Delete/5
-        [HttpPost, ActionName("Delete")]
-        [ValidateAntiForgeryToken]
-        public ActionResult DeleteConfirmed(int id)
-        {
-            Ingresos ingresos = db.Ingresos.Find(id);
-            db.Ingresos.Remove(ingresos);
-            db.SaveChanges();
-            return RedirectToAction("Index");
         }
 
         protected override void Dispose(bool disposing)
