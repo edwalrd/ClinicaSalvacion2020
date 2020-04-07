920d51d14082b4affc487bba5579daea8a7fc010
edwal <edwallg@gmail.com>
2020-04-07 09:30:22 -0400

    algunos problemas resuelto de los filtros y algunos cambio implementado

--- clinicasalvacion2020/clinicasalvacion2020/Models/Altamedicas.cs
+++ clinicasalvacion2020/clinicasalvacion2020/Models/Altamedicas.cs
@@ -17,19 +17,25 @@ namespace clinicasalvacion2020.Models
         public string nombrep { get; set; }
 
         [Display(Name = "Fecha de ingreso")]
+        [StringLength(30, ErrorMessage = "No puede pasar de 30 caracteres")]
 
         public string fechaingreso { get; set; }
 
         [Display(Name = "Habitacion")]
+        [Required(ErrorMessage = "Este campo no puede estar vacio")]
+        [StringLength(30, ErrorMessage = "No puede pasar de 30 caracteres")]
+
 
         public string habitacion { get; set; }
 
         [Display(Name = "Fecha de salida")]
+        [Required(ErrorMessage = "Este campo no puede estar vacio")]
+        [StringLength(30, ErrorMessage = "No puede pasar de 30 caracteres")]
+
 
         public string fechasalida { get; set; }
         [Display(Name = "Monto")]
         public int monto { get; set; }
-
 
         public int ingresoid { get; set; }
         [ForeignKey("ingresoid")]
